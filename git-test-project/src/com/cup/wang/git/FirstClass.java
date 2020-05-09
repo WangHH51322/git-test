e4b4cc1fc159d84fe64b7fd49555a21b627da6d0
WangHH51322 <1252011170@qq.com>
2020-05-09 12:10:26 +0800

    从仓库克隆并修改

--- git-test-project/src/com/cup/wang/git/FirstClass.java
+++ git-test-project/src/com/cup/wang/git/FirstClass.java
@@ -4,9 +4,11 @@ public class FirstClass {
     public static void main(String[] args) {
 
         System.out.println("Hello Git Project!!!");
+        System.out.println("Hello World！！！");
     }
     public void test(){
         int i = 0;
         i += 1;
+        System.out.println(i);
     }
 }
